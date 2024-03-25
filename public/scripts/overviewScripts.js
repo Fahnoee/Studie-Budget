c07c45eb3b174bcf1bfba429591c7d848b0a65e0
Magnus Emil Wichmann <magnuswich@live.dk>
2024-03-25 13:57:08 +0100

    First version of input popup

---
+++ public/scripts/overviewScripts.js
@@ -0,0 +1,13 @@
+console.log("LUUUUUUUUUUUURT");
+
+const showPopup = document.querySelector('.show-popup');
+const popupContainer = document.querySelector('.popup-container');
+const closeBtn = document.querySelector('.close-btn');
+
+showPopup.onclick = () => {
+    popupContainer.classList.add('active');
+}
+
+closeBtn.onclick = () => {
+    popupContainer.classList.remove('active');
+}
