dbd376181676cdff96c640c259fa32671c096041
AugustHG <augi@h257.aau1x-2.klient.sydhavn.site.aau.dk>
2024-03-26 14:15:56 +0100

    input feature men ikke færdig

--- public/javascripts/overviewScripts.js
+++ public/javascripts/overviewScripts.js
@@ -1,9 +1,12 @@
-console.log("LUUUUUUUUUUUURT");
 
 const showPopup = document.querySelector('.show-popup');
 const popupContainer = document.querySelector('.popup-container');
 const body = document.querySelector('body');
 const closeBtn = document.querySelector('.close-btn');
+const saveBtn = document.querySelector('.save-btn');
+
+const income = document.querySelector('.income');
+const expense = document.querySelector('.expense');
 
 showPopup.onclick = () => {
     popupContainer.classList.add('active');
@@ -12,3 +15,25 @@ showPopup.onclick = () => {
 closeBtn.onclick = () => {
     popupContainer.classList.remove('active');
 }
+
+saveBtn.onclick = () => {
+
+    //////////////////////////////////////////////HER SKAL DER INDSÆTTES RIGTIGE BRUGER NAVN
+    let username;
+    username = "John Doe";
+
+    let incomeVal = income.value;
+    let expenseVal = expense.value;
+    console.log(incomeVal, expenseVal);
+
+    /*try {
+        await budgetFunctions.updateBudget(username, {     ///bruger username til at updatere budgetten.
+            income: incomeVal,
+            expenses: expenseVal,
+        });
+        popupContainer.classList.remove('active');
+    } catch (error){
+        console.log("fejl:" + error);
+    }*/
+    popupContainer.classList.remove('active');
+};
