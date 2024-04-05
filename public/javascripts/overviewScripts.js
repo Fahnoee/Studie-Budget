e0caacb61431fa797f39214c1fd62729088e3778
Kevin <148341253+KevinPedersenAAU@users.noreply.github.com>
2024-04-05 12:37:16 +0200

    Added pie chart

--- public/javascripts/overviewScripts.js
+++ public/javascripts/overviewScripts.js
@@ -4,9 +4,13 @@ const popupContainer = document.querySelector('.popup-container');
 const body = document.querySelector('body');
 const closeBtn = document.querySelector('.close-btn');
 const saveBtn = document.querySelector('.save-btn');
+const pie = document.querySelector('div.pie');
 
 const income = document.querySelector('.income');
 const expense = document.querySelector('.expense');
+
+// Set current percentage to pie chart when loading the page
+// setPiePercentage(40);
 
 showPopup.onclick = () => {
     popupContainer.classList.add('active');
@@ -36,4 +40,17 @@ saveBtn.onclick = () => {
         console.log("fejl:" + error);
     }*/
     popupContainer.classList.remove('active');
+
+    // Save changed income/expense to pie chart
+    let getPieStyle = getComputedStyle(pie)
+    let getPieValue = getPieStyle.getPropertyValue('--p');
+    console.log("The value of --p is: " + getPieValue);
+
+    setPiePercentage(20);
 };
+
+function setPiePercentage(percent) {
+    // Set the value of variable --p to another value (in this case 20)
+    pie.style.setProperty('--p', percent);
+    console.log("The value of --p is: " + pie.style.getPropertyValue('--p'));
+  }
