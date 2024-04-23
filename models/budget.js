a27be8c937b6eb84819819b7764ecef85de5d313
Mikkel Martinus Fahnøe <mikkel@h66.aau1x-2.klient.sydhavn2.site.aau.dk>
2024-04-23 13:23:38 +0200

    month scheme

--- models/budget.js
+++ models/budget.js
@@ -3,13 +3,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const BudgetSchema = new Schema({
-  income: { type: Number, required: true },
-  expenses: { type: Number, required: true },
-
-  savings: { type: Number, required: true },
-  customExpenses: { type: mongoose.Schema.Types.Mixed, default: {} }, // Updated line
-  customIncomes: { type: mongoose.Schema.Types.Mixed, default: {} }
-  
+  monthlyRecords: [{
+    month: { type: String, required: true },
+    year: { type: String, required: true },
+    income: { type: Number, required: true },
+    expenses: { type: Number, required: true },
+    savings: { type: Number, required: true },
+    customExpenses: { type: mongoose.Schema.Types.Mixed, default: {} },
+    customIncomes: { type: mongoose.Schema.Types.Mixed, default: {} }
+  }]
 });
 
 // Export model
