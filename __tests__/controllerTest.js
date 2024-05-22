8ec588575f26de1caf31bcaf162d980e427c7d43
Magnus Emil Wichmann <magnuswich@live.dk>
2024-05-22 13:51:37 +0200

    Commenting code in controllerTests.js & app.js

--- __tests__/controllerTest.js
+++ __tests__/controllerTest.js
@@ -5,23 +5,22 @@ const Budget = require('../models/budget');
 
 describe('Budget Controller', () => {
 
-  beforeEach(async () => {
-    // Clear the database before each test
+  beforeEach(async () => {            // Clear the database before each test
     await User.deleteMany({});
     await Budget.deleteMany({});
   });
 
-  describe('createUserWithBudget', () => {
+  describe('createUserWithBudget', () => {  // Testing createUserWithBudget function from budgetController.js
     test('should create a user with a default budget', async () => {
-      const username = 'TestUser';
-      const password = 'password';
-      const user = await createUserWithBudget(username, password);
+      const username = 'TestUser';      // Create dummy username
+      const password = 'password';      // Create dummy password
+      const user = await createUserWithBudget(username, password); // Call the function with dummy username and pass and put response into "user" variable
 
-      expect(user).toHaveProperty('name', username.toLowerCase());
-      expect(user).toHaveProperty('password', password);
-      expect(user).toHaveProperty('budget');
+      expect(user).toHaveProperty('name', username.toLowerCase()); // Expect user.name to be "TestUser" in lowercase
+      expect(user).toHaveProperty('password', password);           // Expect user.password to exist
+      expect(user).toHaveProperty('budget');                       // Expect user to have .budget property
 
-      const budget = await Budget.findById(user.budget);
+      const budget = await Budget.findById(user.budget);           // Expect property budget to have the following key and values
       expect(budget.monthlyRecords[0].income).toBe(0);
       expect(budget.monthlyRecords[0].expenses).toBe(0);
       expect(budget.monthlyRecords[0].savings).toBe(0);
@@ -29,41 +28,40 @@ describe('Budget Controller', () => {
     });
   });
 
-  describe('fetchUserBudgetId', () => {
+  describe('fetchUserBudgetId', () => { // Testing fetchUserBudgetId  from budgetController.js
     test('should fetch the correct budget ID for a given user', async () => {
       const username = 'TestUser';
       const password = 'password';
-      await createUserWithBudget(username, password);
-      const budgetId = await fetchUserBudgetId(username.toLowerCase());
+      await createUserWithBudget(username, password);                     // Needs to create a user before being able 
+      const budgetId = await fetchUserBudgetId(username.toLowerCase());   // Fetch budget id from budget object with username
 
-      const user = await User.findOne({ name: username.toLowerCase() });
-      expect(budgetId.toString()).toEqual(user.budget.toString());
+      const user = await User.findOne({ name: username.toLowerCase() });  // Fetch budget id from user object with username
+      expect(budgetId.toString()).toEqual(user.budget.toString());        // Expect budgetID to equal to value of property .budget from user
     });
 
-    test('should throw an error if the user does not exist', async () => {
-      await expect(fetchUserBudgetId('NonExistentUser')).rejects.toThrow('No user found with name NonExistentUser');
+    test('should throw an error if the user does not exist', async () => {  // Expect error if something is not working
+      await expect(fetchUserBudgetId('NonExistentUser')).rejects.toThrow('No user found with name NonExistentUser'); 
     });
   });
 
-  describe('updateMonthlyBudget', () => {
+  describe('updateMonthlyBudget', () => {  // Testing updateMonthlyBudget function from budgetController.js
     test('should update monthly budget correctly', async () => {
 
-      // Get current month and year
-      const currentDate = new Date();
+      const currentDate = new Date();                  // Get current month and year
       const currentMonth = currentDate.getMonth() + 1; // JavaScript months are 0-indexed
       const currentYear = currentDate.getFullYear();
-      // Mock data
+      
       const username = 'TestUser';
       const password = 'password';
-      const data = { income: 1000, expenses: 500, savings: 500 };
+      const data = { income: 1000, expenses: 500, savings: 500 };  // Create dummy fixed monthly data
 
       const user = await createUserWithBudget(username, password);
       await updateMonthlyBudget(username.toLowerCase(), currentMonth, currentYear, data);
-      const budget = await Budget.findById(user.budget);
+      const budget = await Budget.findById(user.budget);  // Fetch budget
 
-      expect(budget.monthlyRecords[0].income).toBe(data.income);
-      expect(budget.monthlyRecords[0].expenses).toBe(data.expenses);
-      expect(budget.monthlyRecords[0].savings).toBe(data.savings);
+      expect(budget.monthlyRecords[0].income).toBe(data.income);      // Expect income data to be same as given above  
+      expect(budget.monthlyRecords[0].expenses).toBe(data.expenses);  // Expect expense data to be same as given above 
+      expect(budget.monthlyRecords[0].savings).toBe(data.savings);    // Expect savings data to be same as given above 
     });
 
     test('should throw an error if the newData is not an object', async () => {
@@ -75,46 +73,44 @@ describe('Budget Controller', () => {
       
       await createUserWithBudget(username, password);
       
-      await expect(updateMonthlyBudget(username.toLowerCase(), currentMonth, currentYear, { testData: null })).rejects.toThrow('One or more of the data fields are empty');
+      await expect(updateMonthlyBudget(username.toLowerCase(), currentMonth, currentYear, { testData: null })).rejects.toThrow('One or more of the data fields are empty'); // If an error is thrown, expect this error message
     });
   });
 
-  describe('addCustomExpense', () => {
+  describe('addCustomExpense', () => { // Testing addCustomExpense function from budgetController.js
     test('should add a custom expense to the user\'s budget', async () => {
       const username = 'TestUser';
       const password = 'password';
       await createUserWithBudget(username, password);
-      const category = 'Food';
-      const items = [{ name: 'Pizza', cost: 20 }];
+      const category = 'Food';                        // Creating dummy data
+      const items = [{ name: 'Pizza', cost: 20 }];    // Creating dummy data
 
       const updatedBudget = await addCustomExpense(username.toLowerCase(), { category, items });
-      expect(updatedBudget.customExpenses[category]).toEqual(expect.arrayContaining(items));
+      expect(updatedBudget.customExpenses[category]).toEqual(expect.arrayContaining(items));  // Expect the customExpense property to have a property named 'category' containing the items array
     });
     test('should throw an error if the user does not exist', async () => {
-      const category = 'Food';
-      const items = [{ name: 'Pizza', cost: 20 }];
-    
+      const category = 'Food';                        // Creating dummy data
+      const items = [{ name: 'Pizza', cost: 20 }];    // Creating dummy data
+      // Attempt to add custom expense to a user that doesnt exist and expect error throw message
       await expect(addCustomExpense('NonExistentUser', { category, items })).rejects.toThrow('Problem adding custom expense for NonExistentUser: Error finding UserID for NonExistentUser: No user found with name NonExistentUser');
     });
 });
-  describe('deleteUser', () => {
+  describe('deleteUser', () => {   // Testing deleteUser function from budgetController.js
     test('should delete a user and their associated budget', async () => {
       const username = 'TestUser';
       const password = 'password';
       await createUserWithBudget(username, password);
 
-      // Fetch the user to get the budget ID before deletion
-      const existingUser = await User.findOne({ name: username.toLowerCase() });
+      const existingUser = await User.findOne({ name: username.toLowerCase() });        // Fetch the user to get the budget ID before deletion
       const budgetId = existingUser.budget;
 
       await deleteUser(username.toLowerCase());
 
-      const userAfterDeletion = await User.findOne({ name: username.toLowerCase() });
-      expect(userAfterDeletion).toBeNull();
+      const userAfterDeletion = await User.findOne({ name: username.toLowerCase() });  // Attempt to find deleted user and put response in "userAfterDeletion"
+      expect(userAfterDeletion).toBeNull();                                            // Expect userAfterDeletion to be null
 
-      // Use the previously fetched budgetId to check if the budget has been deleted
-      const budgetAfterDeletion = await Budget.findById(budgetId);
-      expect(budgetAfterDeletion).toBeNull();
+      const budgetAfterDeletion = await Budget.findById(budgetId);                     // Attempt to find deleted budget and put response in "budgetAfterDeletion"
+      expect(budgetAfterDeletion).toBeNull();                                          // Expect budgetAfterDeletion to be null
     });
   });
 });
--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const createError = require("http-errors");
+const createError = require("http-errors");   // Express standard requires
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const User = require("./models/user");
 
-const session = require('express-session');
+const session = require('express-session');   // Including express session library
 
 const express = require("express");
-const bodyParser = require("body-parser");
-require('dotenv').config();
+const bodyParser = require("body-parser");    //Module for converting JSON formatted packages
+require('dotenv').config();                   // dotenv module for saving mongodb connection string outside of codebase
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(session({
+app.use(session({                       // express-session setup with dotenv saved passkey
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }));
-const overviewRouter = require("./routes/overview");
+
+const overviewRouter = require("./routes/overview");  //Include routes for directing
 const financialTipsRouter = require("./routes/financialTips");
 const frontpageRouter = require("./routes/frontpage");
 const signUpRouter = require("./routes/signUpSide");
@@ -27,9 +27,7 @@ const logInRouter = require("./routes/logInSite");
 const helpSiteRouter = require("./routes/helpSite");
 const settingRouter = require("./routes/settingSite");
 
-const controller = require("./controllers/budgetController.js");
-
-
+const controller = require("./controllers/budgetController.js");  //Controller (functions) for handling packages
 
 //###########################
 // Set up mongoose connection
@@ -46,61 +44,51 @@ async function main() {
 //############################ 
 // UPDATING DATA IN MONGODB 
 //############################ 
-// Adding custom expense to MongoDB
-
-app.post("/api/addcustom/expense", (req, res) => {
-  // Get data sent from the frontend
-  let data = req.body;
+app.post("/api/addcustom/expense", (req, res) => {  //Add an expense in
+  let data = req.body;                     // Get data sent from the frontend
 
   let username = data.username;
-  let expenseData = {
+  
+  let expenseData = {                       // Pack data for controller
     category: data.category,
     items: data.customExpense
   }
   let newName = data.newName;
 
-  // Call your controller's method
-  controller
+  controller                                 // Call to controller's method
     .addCustomExpense(username, expenseData, newName)
-    .then((result) => {
-      // Budget update successful
+    .then((result) => {                      // Budget update successful handling
       console.log("Result: \n" + result);
       res.json({ message: "Expense added successfully: \n" + result });
     })
-    .catch((err) => {
-      // If anything goes wrong
-      res.status(500).json({ message: "Error in adding expense." });
+    .catch((err) => {                        //Catch potential errors
+      res.status(500).json({ message: "Error in adding expense:", err });
     });
 });
 
 // Adding custom income to MongoDB
 
 app.post("/api/addcustom/income", (req, res) => {
-  // Get data sent from the frontend
   let data = req.body;
-
   let username = data.username;
+
   let incomeData = {
     category: data.category,
     items: data.customIncome
   }
 
-  // Call your controller's method
   controller
     .addCustomIncome(username, incomeData)
     .then((result) => {
-      // Budget update successful
       console.log("Result: \n" + result);
       res.json({ message: "Income added successfully: \n" + result });
     })
     .catch((err) => {
-      // If anything goes wrong
       res.status(500).json({ message: "Error in adding income." });
     });
 });
 
-// Deleting custom inputs from database
-app.post("/api/deletecustom", (req,res) => {
+app.post("/api/deletecustom", (req,res) => {  // Deleting custom inputs from database
   let data = req.body;
 
   let username = data.username;
@@ -111,18 +99,15 @@ app.post("/api/deletecustom", (req,res) => {
   controller
     .deleteCustom(username, deleteData)
     .then((result) => {
-      // Budget update successful
       console.log("Budget update successful....Result: \n" + result);
       res.json({ message: "Custom data deleted successfully: \n" + result });
     })
     .catch((error) => {
-      // If anything goes wrong
       res.status(500).json({ message: "Error deleting custom income/expense.", error });
     });
 });
 
-// Deleting categories from database
-app.post("/api/deletecategory", (req,res) => {
+app.post("/api/deletecategory", (req,res) => {  // Deleting categories from database
   let data = req.body;
 
   let username = data.username;
@@ -130,28 +115,23 @@ app.post("/api/deletecategory", (req,res) => {
   controller
     .deleteCategory(username, categoryName)
     .then((result) => {
-      // Budget update successful
       console.log("Budget update successful....Result: \n" + result);
       res.json({ message: "Category deleted successfully: \n" + result });
     })
     .catch((error) => {
-      // If anything goes wrong
       res.status(500).json({ message: "Error deleting category.", error });
     });
 });
 
-app.post("/api/deleteuser", (req,res) => {
+app.post("/api/deleteuser", (req,res) => {  //Delete user from database
   let data = req.body;
-  console.log("TEST HER", data.username);
   controller
     .deleteUser(data.username)
     .then((result) => {
-      // Budget update successful
       console.log("User deleted successfully: \n" + result);
       res.json({ message: "User deleted successfully: \n" + result });
     })
     .catch((error) => {
-      // If anything goes wrong
       res.status(500).json({ message: "Error deleting user.", error });
     });
 });
@@ -165,91 +145,80 @@ app.post("/api/logout", (req,res) => {
 //###########################
 // READING DATA FROM MONGO-DB
 //###########################
-// Defining our GET endpoint --- Define route to fetch data for a specific user
-app.get('/api/budget/:budgetID', async (req, res) => {
+app.get('/api/budget/:budgetID', async (req, res) => {             //Fetches budgetID from the database
   try {
-      // Query MongoDB to retrieve data for the specific user
-      let username = req.session.username;                                    //RET TIL BRUG AF RIGTIGT USERNAME
-      const id = await controller.fetchUserBudgetId(username);                // Using function from budgetcontroller.js to find user's id
-      const data = await controller.Budget.findOne({ _id: id });              // Using user id to find their budget 
-      if (!data) {
-          return res.status(404).json({ message: "Data not found for the specified user" });      // Error if file contains no data (!data)
+      let username = req.session.username;                         // Query MongoDB to retrieve data for the specific user
+      const id = await controller.fetchUserBudgetId(username);     // Using function from budgetcontroller.js to find user's id
+      const data = await controller.Budget.findOne({ _id: id });   // Using budget id to find their budget 
+      if (!data) {                                                 // If file contains data, respond with 404 message
+          return res.status(404).json({ message: "Data not found for the specified user" });
       }
-      res.json(data); // If file contains data, respond with that data
+      res.json(data);                                              // If file contains data, respond with that data
   } catch (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
+      res.status(500).send('Internal Server Error:', err);
   }
 });
 
-
-app.post('/login', async (req, res) => {
+app.post('/login', async (req, res) => {  //Logs a user in
   let data = req.body;
   let username = data.username;
   let password = data.password;
   try {
-    await controller.findUserByUsernameAndPassword(username.toLowerCase(), password);
+    await controller.findUserByUsernameAndPassword(username.toLowerCase(), password); // Tries to find the user
     req.session.username = username.toLowerCase();
-    res.redirect('/overview'); // Redirect to the overview page if user login is successful
+    res.redirect('/overview');                                                        // Redirect to the overview page if user login is successful
 
   } catch (error) {
-    if (error.message == 'Error finding user: User not found or password incorrect') {
-      // Render a view with a retry option and an error alert
+    if (error.message == 'Error finding user: User not found or password incorrect') {// Render a view with a retry option and an error alert
       res.render('logInSite', { 
         error: 'Login is invalid, please try again.', 
         retryUrl: '/logIn',
-        alert: 'Error: Password or Username is incorrect'
+        alert: 'Error: Password or Username is incorrect'                             // Error message passed to view for display on site.
       });
-    } else {
-      // Handle other errors
+    } else {                                                                          // Handle other errors
       console.error(error);
       res.status(500).send('An error occurred during the log in process.');
     }
   }
 });
 
-
-
-
-// Route to handle POST request from the signup form
-app.post('/signup', async (req, res) => {
+app.post('/signup', async (req, res) => { //Sign up
   let data = req.body;
   let username = data.username;
   let password = data.password;
 
   try {
-    await controller.createUserWithBudget(String(username), String(password));
-    req.session.username = username.toLowerCase();
-    res.redirect('/overview?startTutorial=true');
+    await controller.createUserWithBudget(String(username), String(password));  //Controller function that creates new user
+    req.session.username = username.toLowerCase();                              //Username to lowercase
+    res.redirect('/overview?startTutorial=true');                               // When SignUp successful, redirect to overview page and start tutorial
     
   } catch (error) {
-    if (error.message === 'User already exists') {
-      // Render a view with a retry option and an error alert
+    if (error.message === 'User already exists') {                              // Render a view with a retry option and an error alert
       res.render('signUpSide', { 
         error: 'User already exists. Please choose a different username.', 
         retryUrl: '/signup',
         alert: 'Error: User already exists. Try a different username.'
       });
     } else {
-      // Handle other errors
+                                                                                // Handle other errors
       console.error(error);
       res.status(500).send('An error occurred during the signup process.');
     }
   }
 });
 
-app.get('/api/customexpenses/:month/:year', async (req, res) => {
+app.get('/api/customexpenses/:month/:year', async (req, res) => { // Calculate expenses for a given month/year
   try {
     const username = req.session.username;
-    const { month, year } = req.params;
+    const { month, year } = req.params;                                                           //Takes parameters from the api-endpoint
     const expenses = await controller.fetchCustomExpensesByMonthAndYear(username, month, year);
-    res.json(expenses);
+    res.json(expenses);                                                                            //If successful, respond with expenses for given month/year
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-app.get('/api/customeincomes/:month/:year', async (req, res) => {
+app.get('/api/customeincomes/:month/:year', async (req, res) => { // Calculate incomes for a given month/year
   try {
     const username = req.session.username;
     const { month, year } = req.params;
@@ -260,18 +229,18 @@ app.get('/api/customeincomes/:month/:year', async (req, res) => {
   }
 });
 
-app.get('/api/monthlybudget/:month/:year', async (req, res) => {
+app.get('/api/monthlybudget/:month/:year', async (req, res) => { // Calculates a collected budget for a given month
   try {
     const username = req.session.username;
     const { month, year } = req.params;
-    const monthlyBudget = await controller.getMonthlyBudget(username, parseInt(month), parseInt(year));
-    res.json(monthlyBudget);
+    const monthlyBudget = await controller.getMonthlyBudget(username, parseInt(month), parseInt(year)); // parseInt to convert strings to integers
+    res.json(monthlyBudget);                                                                            // If successful, respond with the budget for that month and year
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-app.post('/api/update_monthly_budget', async (req, res) => {
+app.post('/api/update_monthly_budget', async (req, res) => { // Alters a budget from a given month
   try {
     const username = req.session.username;
     const { month, year, income, expenses, savings } = req.body;
@@ -282,21 +251,16 @@ app.post('/api/update_monthly_budget', async (req, res) => {
   }
 });
 
-// View engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");   // Set pug as the view engine
+app.set("views", path.join(__dirname, "views"));  // Pug view engine setup
+app.set("view engine", "pug");
 
-
-// Middleware setup
-app.use(logger("dev"));
+app.use(logger("dev"));    // Middleware setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-
-// Routes setup
-app.use("/", frontpageRouter);
+app.use("/", frontpageRouter);  // Routes setup
 app.use("/overview", overviewRouter);
 app.use("/financialTips", financialTipsRouter);
 app.use("/signup", signUpRouter);
@@ -304,20 +268,15 @@ app.use("/login", logInRouter);
 app.use("/Help", helpSiteRouter);
 app.use("/Settings", settingRouter);
 
-// Catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function (req, res, next) { // Catch 404 and forward to error handler
   next(createError(404));
 });
 
-
-// Error handler
-app.use(function (err, req, res, next) {
-  // Set locals, only providing error in development
-  res.locals.message = err.message;
+app.use(function (err, req, res, next) {  // Error handler
+  res.locals.message = err.message;      // Set locals, only providing error in development
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // Render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 500);    // Render the error page
   res.render("error");
 });
 
